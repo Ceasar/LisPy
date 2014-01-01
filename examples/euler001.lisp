7fd041efbfa22833d3ece2e00e68f7513a33bda1
Ceasar Bautista <cbautista2010@gmail.com>
2013-12-31 22:16:49 -0500

    correct example

--- examples/euler001.lisp
+++ examples/euler001.lisp
@@ -1,7 +1,10 @@
-(do
-  (:= foldr (\ (f z xs) (if (== (length xs) 0) z (f (head xs) (foldr f z (tail xs))))))
-  (:= sum (\ (xs) (foldr + 0 xs)))
-  (:= divides (\ (a b) (== (% a b) 0)))
-  (:= range (\ (n) (if (== n 0) [] (: n (range (- n 1))))))
+(begin
+  (define (foldr f z xs)
+          (if (== (length xs) 0)
+              z
+              (f (head xs) (foldr f z (tail xs)))))
+  (define (sum xs) (foldr + 0 xs))
+  (define (divides a b) (== (% a b) 0))
+  (define (range n) (if (== n 0) () (: n (range (- n 1)))))
   (sum (range 100))
 )
