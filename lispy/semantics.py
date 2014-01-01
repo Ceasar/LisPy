a624e68d67d8ee620e23a9d9363ae573d1137bf7
Ceasar Bautista <cbautista2010@gmail.com>
2014-01-01 00:26:04 -0500

    WIP

--- lispy/semantics.py
+++ lispy/semantics.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 from expression import Atom
 from context import Context
 
@@ -9,8 +11,10 @@ def do_if(context, test, consequence, alternative):
 
 def do_define(context, name, value):
     try:
-        context[name.operator] = lambda *args: (
-            evaluate(value, Context(name.operands, args, context)))
+        def f(*args):
+            return  evaluate(value, Context(name.operands, args, context))
+        f.__name__ = str(name.operator)
+        context[name.operator] = f
     except AttributeError:
         context[name] = evaluate(value, context)
 
@@ -28,6 +32,51 @@ BUILTINS = {
 }
 
 
+def get_value(expression, context):
+    if type(expression) == Atom:
+        try:
+            return int(expression.name)
+        except ValueError:
+            try:
+                return float(expression.name)
+            except ValueError:
+                return context.find(expression.name)[expression.name]
+    else:
+        if len(expression.elements) == 0:
+            return []
+        try:
+            return BUILTINS[expression.operator.name](context, *expression.operands)
+        except KeyError:
+            raise ValueError("not a value")
+
+def evaluate(expression, context):
+    expressions = deque([expression])
+    values = deque()
+    while expressions:
+        expression = expressions.popleft()
+        try:
+            value = get_value(expression, context)
+        except ValueError:
+            expressions.extendleft(expression.elements)
+        else:
+            values.appendleft(value)
+    arguments = deque()
+    while len(values) > 1:
+        value = values.pop()
+        if callable(value):
+            procedure = value
+            print procedure, arguments
+            values.append(procedure(*arguments))
+            arguments.clear()
+        else:
+            arguments.appendleft(value)
+    if arguments:
+        return values[0](*arguments)
+    else:
+        return values[0]
+
+
+"""
 def evaluate(expression, context):
     if type(expression) == Atom:
         try:
@@ -43,6 +92,8 @@ def evaluate(expression, context):
         try:
             return BUILTINS[expression.operator.name](context, *expression.operands)
         except KeyError:
-            procedure = evaluate(expression.operator, context)
-            arguments = (evaluate(operand, context) for operand in expression.operands)
+            values = evaluate((element for element in expression.elements), context)
+            procedure = values[0]
+            arguments = values[1:]
             return procedure(*arguments)
+"""
